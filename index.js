129870b701cbaa1e9f3be38c93f1ae1fd3e3e5a5
Ashton Reed <dreamergirl411@gmail.com>
2017-11-30 16:30:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const drivers = ["Milo", "Otis", "Garfield"];
-let destructivelyAppendDriver = drivers.push("Ralph");
-drivers;
+function destructivelyAppendDriver() {
+  return drivers.push("Ralph");
+}
